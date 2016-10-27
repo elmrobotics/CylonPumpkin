abeb835d7a7a366a8fa2ffebf1e54ddeeb52f35f
Eric Erickson <steamedcotton@gmail.com>
2016-10-26 20:53:58 -0700

    Updated to work with Marks pumpkin

--- CylonPumpkin.ino
+++ CylonPumpkin.ino
@@ -1,131 +1,62 @@
 #include <Adafruit_NeoPixel.h>
-#include <avr/power.h>
 
-#define PIN 6
+#define PIN 6 //The Pin out your Neopixel DIN strip/stick is connected to (Default is 6)
+#define TPIXEL 10 //The total amount of pixel's/led's in your connected strip/stick (Default is 60)
+int wait_T=80; //This is the delay between moving back and forth and per pixel
+int wait_E=60; // How long to wait at the ends
+int PixelCount=10; //Set this to the AMOUNT of Led's/Pixels you have or want to use on your strip And It can be used to tell where to Stop then return the eye at in the strip
+int Pixel_Start_End=0; //Set this to where you want it to Start/End at
+boolean UsingBar = false; //Set this to true If you are using the 8x1 Neopixel Bar Or you want to only use 3 leds for the scanner. 
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(10, PIN, NEO_GRB + NEO_KHZ800);
-
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(TPIXEL, PIN, NEO_GRB + NEO_KHZ800); //Standered Strip function
 
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-#if defined (__AVR_ATtiny85__)
-  if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-#endif
-  // End of trinket special code
-
-
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+  //Serial.begin(9600); //Used For pixel Count Debugging
 }
-
 void loop() {
-  // Some example procedures showing how to display to the pixels:
-  colorWipe(strip.Color(255, 0, 0), 50); // Red
-  colorWipe(strip.Color(0, 255, 0), 50); // Green
-  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pixel chase in...
-  theaterChase(strip.Color(127, 127, 127), 50); // White
-  theaterChase(strip.Color(127,   0,   0), 50); // Red
-  theaterChase(strip.Color(  0,   0, 127), 50); // Blue
-
-  rainbow(20);
-  rainbowCycle(20);
-  theaterChaseRainbow(50);
+  //Example: CylonEyeUp(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeUp(strip.Color(175,0,0), strip.Color(25,0,0), strip.Color(10,0,0), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_E);
+  //Example: CylonEyeDown(Center_Dot_Color, Second_Dot_color, Third_Dot_color, wait_T, PixelCount, Pixel_Start_End);
+  CylonEyeDown(strip.Color(175,0,0), strip.Color(25,0,0), strip.Color(10,0,0), wait_T, PixelCount, Pixel_Start_End);
+  delay(wait_E);
 }
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, c);
-      strip.show();
-      delay(wait);
-  }
-}
+void CylonEyeUp(uint32_t Co, uint32_t Ct, uint32_t Ctt, uint8_t Delay, int TotalPixels, int pStart) {
+  for(int i=pStart; i<TotalPixels; i++) {
+    if(!UsingBar) { strip.setPixelColor(i+2, Ctt); } //Third Dot Color
+    strip.setPixelColor(i+1, Ct);   //Second Dot Color
+    strip.setPixelColor(i, Co);     //Center Dot Color
+    strip.setPixelColor(i-1, Ct);   //Second Dot Color
+    if(!UsingBar) { strip.setPixelColor(i-2, Ctt); } //Third Dot Color
 
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
+    if(!UsingBar) {
+      strip.setPixelColor(i-3, strip.Color(0,0,0)); //Clears the dots after the 3rd color
+    } else {
+      strip.setPixelColor(i-2, strip.Color(0,0,0)); //Clears the dots after the 2rd color
     }
     strip.show();
-    delay(wait);
+    //Serial.println(i); //Used For pixel Count Debugging
+    delay(Delay);
   }
 }
+void CylonEyeDown(uint32_t Co, uint32_t Ct, uint32_t Ctt, uint8_t Delay, int TotalPixels, int pEnd) {
+  for(int i=TotalPixels-1; i>pEnd; i--) {
+    if(!UsingBar) { strip.setPixelColor(i-2, Ctt); } //Third Dot Color
+    strip.setPixelColor(i-1, Ct);   //Second Dot Color
+    strip.setPixelColor(i, Co);    //Center Dot Color
+    strip.setPixelColor(i+1, Ct);  //Second Dot Color
+    if(!UsingBar) { strip.setPixelColor(i+2, Ctt); } //Third Dot Color
 
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+    if(!UsingBar) { 
+      strip.setPixelColor(i+3, strip.Color(0,0,0)); //Clears the dots after the 3rd color
+    } else {
+      strip.setPixelColor(i+2, strip.Color(0,0,0)); //Clears the dots after the 2rd color
     }
     strip.show();
-    delay(wait);
+    //Serial.println(i); //Used For pixel Count Debugging
+    delay(Delay);
   }
 }
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
-      }
-      strip.show();
-     
-      delay(wait);
-     
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-        }
-        strip.show();
-       
-        delay(wait);
-       
-        for (int i=0; i < strip.numPixels(); i=i+3) {
-          strip.setPixelColor(i+q, 0);        //turn every third pixel off
-        }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else if(WheelPos < 170) {
-    WheelPos -= 85;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  }
-}
-
